37446ebf90b1e0591a1fe992a4c58b1ec36a547f
jeffreyOben <Jeffreyoben17@outlook.com>
2025-12-25 16:18:44 +0000

    add load_data.py

--- data_loader.py
+++ data_loader.py
@@ -1,2 +1,35 @@
+import csv
+
 def load_data(file_path):
-	print("loading csv file...")
+    print(f"\nLoading CSV file from {file_path}...")
+    data = []
+
+    try:
+        with open(file_path, mode="r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append(row)
+
+        print(f"SUCCESS: {len(data)} rows loaded.")
+
+    except FileNotFoundError:
+        print(f"ERROR: File not found: {file_path}")
+        data = None
+
+    except Exception as e:
+        print(f"ERROR: {e}")
+        data = None
+
+    input("\nPress Enter to return to the main menu...")
+    return data
+
+# Only job: load and prepare data
+
+# Responsibilities:
+# Read CSV
+# Convert types (int, date, datetime)
+# Handle missing values
+# Return structured data (list of dicts)
+
+row_headings = ['video_id', 'trending_date', 'title', 'channel_title', 'category_id', 'publish_time', 'tags', 'views', 'likes',
+             'dislikes', 'comment_count', 'thumbnail_link', 'comments_disabled', 'ratings_disabled', 'video_error_or_removed', 'description']
--- main.py
+++ main.py
@@ -7,40 +7,40 @@ import visualisation
 file_path = "data/youtube_trending_videos.csv"
 
 def main():
-	data = None
+    loaded_data = None
 
-	while True:
-		user_interface.display_menu()
-		choice = user_interface.get_choice()
+    while True:
+        user_interface.display_menu()
+        choice = user_interface.get_choice()
 
-		if choice == "1":
-			data = data_loader.load_data(file_path)
-		elif choice == "2":
-			if data:
-				data_processing.basic_processing(data)
-			else:
-				print("\n !!!Please load data first.\n")
-		elif choice == "3":
-			if data: 
-				data_processing.intermediate_processing(data)
-			else:
-				print("\n !!!Please load data first.\n")
-		elif choice == "4":
-			if data:
-				file_path
-				visualisation.visualise(data)
-			else:
-				print("\n !!!Please load data first.\n")
-		elif choice == "5":
-			if data:
-				exporter.export_data(data)
-			else:
-				print("\n !!!Please load data first.\n")
-		elif choice == "6":
-			print("\nExiting the program...")
-			break
-		else:
-			print("!!!Invalid choice. Please try again. choices 1-6\n")
+        if choice == "1":
+            loaded_data = data_loader.load_data(file_path)
+        elif choice == "2":
+            if loaded_data:
+                data_processing.basic_processing(loaded_data)
+            else:
+                print("\n !!!Please load data first.\n")
+        elif choice == "3":
+            if loaded_data:
+                data_processing.intermediate_processing(loaded_data)
+            else:
+                print("\n !!!Please load data first.\n")
+        elif choice == "4":
+            if loaded_data:
+                file_path
+                visualisation.visualise(loaded_data)
+            else:
+                print("\n !!!Please load data first.\n")
+        elif choice == "5":
+            if loaded_data:
+                exporter.export_data(loaded_data)
+            else:
+                print("\n !!!Please load data first.\n")
+        elif choice == "6":
+            print("\nExiting the program...")
+            break
+        else:
+            print("!!!Invalid choice. Please try again. choices 1-6\n")
 
 
 if __name__ == "__main__":
